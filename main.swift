3644fcec586e0dc5aab611527a5bf6e200a89c88
Alexander Demin <ademin@gmail.com>
2024-11-22 17:30:03 +0100

    Change tabs to spaces

--- main.swift
+++ main.swift
@@ -2,24 +2,21 @@ import Foundation
 
 import Vapor
 
-
-
 struct Version : Content {
-	var version: String = "1.0.0"
+    var version: String = "1.0.0"
 }
 
 struct Main {
-
-	static func main() async {
-		do {
-			let app = try await Application.make()
-			app.get("version", use: { _ in Version() })
-			try await app.execute()
-			try await app.asyncShutdown()
-		} catch {
-			print("got error: \(error)")
-		}
-	}
-
+    static func main() async {
+        do {
+            let app = try await Application.make()
+            app.get("version", use: { _ in Version() })
+            try await app.execute()
+            try await app.asyncShutdown()
+        } catch {
+            print("got error: \(error)")
+        }
+    }
 }
+
 _ = await Task{ await Main.main() }.value
