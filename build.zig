38642e962a2838247ea7b9f66f71c3d4cc1fb843
Alexander Demin <ademin@gmail.com>
2024-12-04 16:44:31 +0100

    Simplify zig build

--- build.zig
+++ build.zig
@@ -19,33 +19,11 @@ pub fn build(b: *std.Build) void {
     exe.root_module.addImport("httpz", httpz.module("httpz"));
 
     b.exe_dir = ".";
-
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
-
     run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
 
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_cmd.step);
-
-    // ---
-
-    const test_step = b.step("test", "Run unit tests");
-
-    const unit_tests = b.addTest(.{
-        .root_source_file = b.path("main.zig"),
-    });
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-    test_step.dependOn(&run_unit_tests.step);
 }
